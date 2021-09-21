44f03890b38d06eb94717014bc384ebb1db74b14
strvworks <strv13570@gmail.com>
2021-09-22 01:31:05 +0900

    Added polyfill for web animations api

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', type: 'image/png', href: '/apple-touch-icon-180x180.png' },
       { rel: 'icon', type: 'image/png', href: '/icon-192x192.png' }
-    ]
+    ],
+    script: [{ src: 'https://polyfill.io/v3/polyfill.min.js?features=WebAnimations' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
