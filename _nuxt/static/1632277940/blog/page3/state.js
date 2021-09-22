bc753451c70fcd0555906c84e28e0994783df31d
strvert <strvert@users.noreply.github.com>
2021-09-22 02:32:31 +0000

    deploy: 98fceb2e18cb9d801a384cf862547329588419f2

---
+++ _nuxt/static/1632277940/blog/page3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632277940",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpage3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ef7e9cda"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1632277940/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fblog","\u002F","blog\u002Fpage1","blog\u002Fpage2","blog\u002Fpage3","blog\u002Fpage4","\u002Fblog\u002Fpage1"]})
