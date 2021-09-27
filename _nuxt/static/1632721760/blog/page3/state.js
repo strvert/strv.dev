872cf11f1fa32746870c95ff9e37858d8ab8d57d
strvert <strvert@users.noreply.github.com>
2021-09-27 07:27:22 +0000

    deploy: 1559feaf188f2768478475d3a904fe87a74aa90f

--- _nuxt/static/1632721760/blog/page3/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632721760",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpage3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e5798a87"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1632727614/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632727614",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"18fb0ca8"}},globalRefs:{}}}(null,"\u002F"));
