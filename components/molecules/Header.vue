6e20e97668200fb396fe54f63224349c5bacee3d
strvworks <strv13570@gmail.com>
2021-09-28 20:07:24 +0900

    Improved Header

--- components/molecules/Header.vue
+++ components/molecules/Header.vue
@@ -1,96 +1,102 @@
 <template>
-  <div class="header-main">
-    <nav class="header-nav">
-      <ul>
-        <li class="strvdevlogo">
-          <nuxt-link to="/">
-            <strvdev-logo
-              :transformed="logoTransformed"
-              :logoScale="17"
-              :animEffectTiming="logoAnimEffectTiming"
-            />
-          </nuxt-link>
-        </li>
-        <li class="textlink blogposts">
-          <span>BLOG POSTS</span>
-        </li>
-        <li class="textlink about">
-          <span>ABOUT</span>
-        </li>
-      </ul>
-    </nav>
+  <div class="header-main" :style="cssVariables">
+    <div class="site-content-list">
+      <nav class="sitenav">
+        <site-content-list :wrap="siteContentWrap" row-height="44px" :contents="siteContent" />
+      </nav>
+    </div>
+    <div class="header-grid">
+      <div class="strvdevlogo">
+        <strvdev-logo
+          :transformed="logoTransformed"
+          :logoScale="33"
+          :animEffectTiming="logoAnimEffectTiming"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, useContext, onBeforeUnmount } from '@nuxtjs/composition-api';
+import {
+  defineComponent,
+  onMounted,
+  useContext,
+  onBeforeUnmount,
+  computed,
+} from '@nuxtjs/composition-api';
 import StrvdevLogo from '@/components/atoms/StrvdevLogo.vue';
+import SiteContentList from '@/components/atoms/SiteContentList.vue';
 import { useScrollDirectionEvent } from '@/composables/utils/ScrollEvents';
+import { useResizeEvent } from '@/composables/utils/ResizeEvent';
 
 export default defineComponent({
-  components: { StrvdevLogo },
-  setup(props) {
+  components: { StrvdevLogo, SiteContentList },
+  setup() {
     const logoAnimEffectTiming: EffectTiming = {
       duration: 900,
       easing: 'ease',
       fill: 'forwards',
     };
 
-    const { state: logoTransformed, addEvent, removeEvent } = useScrollDirectionEvent();
+    const { state: logoTransformed } = useScrollDirectionEvent();
+    const { width } = useResizeEvent();
 
-    onMounted(() => {
-      addEvent();
+    const siteContentWrap = computed(() => {
+      if (width.value !== undefined) {
+        return width.value <= 800;
+      }
+      return false;
     });
-    onBeforeUnmount(() => {
-      removeEvent();
-    });
+
     const { app } = useContext();
-    console.log(app.$repositories.navContent.get());
+    const { navContent } = app.$repositories;
+    const siteContent = navContent.get();
+    const headerRowHeight = 44; // px
+    const cssVariables = { '--header-row-height': `${headerRowHeight}px` };
 
-    return { logoAnimEffectTiming, logoTransformed };
+    return { logoAnimEffectTiming, logoTransformed, siteContent, cssVariables, siteContentWrap };
   },
 });
 </script>
 
 <style lang="scss" scoped>
 .header-main {
-  --header-height: 44px;
   background-color: var(--strvdev-palette-1);
   box-shadow: 0px 0px 3px 0px var(--strvdev-palette-3);
   inline-size: 100%;
-  block-size: var(--header-height);
 }
 
-.header-nav {
+.site-content-list {
+  display: flex;
+  justify-content: center;
+}
+
+.header-grid {
+  block-size: var(--header-row-height);
   max-inline-size: 800px;
-  block-size: var(--header-height);
   margin: 0 auto;
 
-  > ul {
-    display: grid;
-    margin: 0;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-areas: 'a b c d e';
-    align-content: center;
-    justify-items: center;
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-areas: 'a b c d e';
+  align-items: start;
+  justify-items: center;
 
-    > .textlink {
-      font-size: 20px;
-      font-weight: bold;
-      line-height: var(--header-height);
-    }
+  > .strvdevlogo {
+    grid-area: c;
+    padding-top: 5px;
+  }
 
-    > .blogposts {
-      justify-self: left;
-      grid-area: a;
-    }
-    > .strvdevlogo {
-      grid-area: c;
-      margin: auto 0;
-    }
-    > .about {
-      justify-self: right;
-      grid-area: e;
+  > .sitenav {
+    grid-area: c;
+
+    position: absolute;
+    top: 0;
+
+    &.open {
+      background-color: red;
     }
   }
 }
