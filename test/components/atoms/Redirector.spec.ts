9c377d3405f51e6683deccdee8cd3338f9f08330
strvworks <strv13570@gmail.com>
2021-09-28 20:02:08 +0900

    ??

--- test/components/atoms/Redirector.spec.ts
+++ test/components/atoms/Redirector.spec.ts
@@ -1,10 +1,4 @@
 import { mount } from '@vue/test-utils';
-import Redirector from '@/components/atoms/Redirector.vue';
 
 describe('Redirector.vue', () => {
-  beforeEach(() => jest.useFakeTimers());
-  afterEach(() => jest.useRealTimers());
-  test('display Redirector', () => {
-      console.log("stub test");
-  });
 });
