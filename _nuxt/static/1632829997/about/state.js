5e7398dbab23995e39e915b500bf9e7f6d6b6e6d
strvert <strvert@users.noreply.github.com>
2021-09-28 16:32:21 +0000

    deploy: d784dfb6de421b5b624604596cc5958ebddc7773

--- _nuxt/static/1632829997/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632829997",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"8ef947e7"}},globalRefs:{}}}("\u002Fabout"));
---
+++ _nuxt/static/1632846711/blog/series-opengl--opengl-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632846711",layout:"blogpost",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fseries-opengl--opengl-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"449f6e4f"}},globalRefs:{}}}(null));
