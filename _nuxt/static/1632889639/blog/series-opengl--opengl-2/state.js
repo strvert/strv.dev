d6182e8b2867c5ed0264612c1d4ff36e50f33261
strvert <strvert@users.noreply.github.com>
2021-09-29 05:34:32 +0000

    deploy: 1484fa504ba2e14233e0283c0ed40e92fd03b144

--- _nuxt/static/1632889639/blog/series-opengl--opengl-2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632889639",layout:"blogpost",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fseries-opengl--opengl-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c4a8d208"}},globalRefs:{}}}(null));
