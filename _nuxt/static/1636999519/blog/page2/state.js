0f09d8844dffa50ad30d008d34b9528530b98fa3
strvert <strvert@users.noreply.github.com>
2021-11-15 18:31:33 +0000

    deploy: 1d5a20f053e0a64fe8782e50e29a127f0edb9ed9

--- _nuxt/static/1636999519/blog/page2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636999519",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpage2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"68290877"}}}}(null));
