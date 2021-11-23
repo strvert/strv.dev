62f002d41753eb7f52a28fe2a820bcbf0d47a7c5
strvert <strvert@users.noreply.github.com>
2021-11-23 17:17:42 +0000

    deploy: 027c6bb8ca292bf437642108e4843ccd4426302b

---
+++ _nuxt/static/1637687843/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637687843",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7c347b2e"}},useState:{undefined:void 0}}}(null));
---
+++ _nuxt/static/1637687843/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637687843",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7c347b2e"}}}}(null,"\u002F"));
