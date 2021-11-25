b30f8806d2bbe3f5ab654f37a3c962a312e24e57
strvert <strvert@users.noreply.github.com>
2021-11-25 19:03:50 +0000

    deploy: 0915b06832a07f4bdd798f13151fac04e81684b3

--- _nuxt/static/1637866392/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637866392",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b569c70"}},useState:{undefined:void 0}}}(null));
--- _nuxt/static/1637866392/blog/cpp--write-an-nbit-adder-using-only-types/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637866392",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fcpp--write-an-nbit-adder-using-only-types",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b569c70"}}}}(null));
