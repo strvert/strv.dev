b2c7c97fafcd4f887b5419d8e6166a4ca05b76b6
strvert <strvert@users.noreply.github.com>
2022-12-01 18:44:25 +0000

    deploy: 422faa326a2b03794c54b2c6552365eaeee229e0

---
+++ _nuxt/static/1669920243/blog/unrealengine--draw-rectangles-lines-on-uv-coords/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669920243",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Funrealengine--draw-rectangles-lines-on-uv-coords",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1a8d43af"}},_asyncData:{},_errors:{}}}(null));
